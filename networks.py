ed9ed244cf1384f4d8d5f42b138c04d63f2102f3
David <david.ye01@hotmail.com>
2023-10-25 11:41:29 +0200

    Updated Siamese network, added normalization

--- networks.py
+++ networks.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 
-class SiameseNetwork(nn.Module):
+class SiameseNetworkSimple(nn.Module):
     def __init__(self):
         super(SiameseNetwork, self).__init__()
         self.fc = nn.Sequential(
@@ -14,3 +14,33 @@ class SiameseNetwork(nn.Module):
     def forward(self, x):
         output = self.fc(x)
         return output
+    
+class SiameseNetworkComplex(nn.Module):
+    def __init__(self):
+        super(SiameseNetwork, self).__init__()
+        self.cnn_layers = nn.Sequential(
+            nn.Linear(21, 64),
+            nn.ReLU(inplace=True),
+            nn.Linear(64, 128),
+            nn.ReLU(inplace=True),
+            nn.Linear(128, 256),
+            nn.ReLU(inplace=True),
+        )
+        self.batch_norm = nn.BatchNorm1d(256)
+        self.fc_layers = nn.Sequential(
+            nn.Linear(256, 128),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.9),
+            nn.Linear(128, 64)
+        )
+
+    def forward_once(self, x):
+        x = self.cnn_layers(x)
+        x = self.batch_norm(x)
+        x = self.fc_layers(x)
+        return x
+
+    def forward(self, input1, input2):
+        output1 = self.forward_once(input1)
+        output2 = self.forward_once(input2)
+        return output1, output2
