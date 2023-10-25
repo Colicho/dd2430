97bfd4eca5f24ff0d7abf3bb2becf64234eedf3a
David <david.ye01@hotmail.com>
2023-10-25 22:02:16 +0200

    Cleanup

--- networks.py
+++ networks.py
@@ -15,32 +15,29 @@ class SiameseNetworkSimple(nn.Module):
         output = self.fc(x)
         return output
     
+
 class SiameseNetworkComplex(nn.Module):
     def __init__(self):
         super(SiameseNetworkComplex, self).__init__()
         self.cnn_layers = nn.Sequential(
             nn.Linear(21, 64),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Linear(64, 128),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Linear(128, 256),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
         )
         self.batch_norm = nn.BatchNorm1d(256)
         self.fc_layers = nn.Sequential(
             nn.Linear(256, 128),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Dropout(0.9),
-            nn.Linear(128, 64)
+            nn.Linear(128, 7)
         )
 
-    def forward_once(self, x):
+    def forward(self, x):
         x = self.cnn_layers(x)
         x = self.batch_norm(x)
-        x = self.fc_layers(x)
-        return x
-
-    def forward(self, input1, input2):
-        output1 = self.forward_once(input1)
-        output2 = self.forward_once(input2)
-        return output1, output2
+        output = self.fc_layers(x)
+        return output
+    
