e9b3e66cea9e46ed074fe92715679d084e0a4411
David <david.ye01@hotmail.com>
2023-11-02 18:10:15 +0100

    Updated model and its results

--- networks.py
+++ networks.py
@@ -19,25 +19,19 @@ class SiameseNetworkSimple(nn.Module):
 class SiameseNetworkComplex(nn.Module):
     def __init__(self):
         super(SiameseNetworkComplex, self).__init__()
-        self.cnn_layers = nn.Sequential(
-            nn.Linear(21, 64),
+        self.fc = nn.Sequential(
+            nn.Linear(21, 512),
             nn.ReLU(),
-            nn.Linear(64, 128),
+            nn.Linear(512, 256),
             nn.ReLU(),
-            nn.Linear(128, 256),
-            nn.ReLU(),
-        )
-        self.batch_norm = nn.BatchNorm1d(256)
-        self.fc_layers = nn.Sequential(
             nn.Linear(256, 128),
             nn.ReLU(),
-            nn.Dropout(0.9),
-            nn.Linear(128, 7)
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 7)
         )
 
     def forward(self, x):
-        x = self.cnn_layers(x)
-        x = self.batch_norm(x)
-        output = self.fc_layers(x)
+        output = self.fc(x)
         return output
     
