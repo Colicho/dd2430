52afbf3fbd5a94eba9a6ad14ecdf85189e2205f5
David <david.ye01@hotmail.com>
2023-11-02 17:13:28 +0100

    Continuity labels, values in files for simulation

--- losses.py
+++ losses.py
@@ -20,13 +20,13 @@ class ContrastiveLoss(nn.Module):
         super(ContrastiveLoss, self).__init__()
         self.l2norm = nn.PairwiseDistance(p=2)
 
-    def forward(self, output_1, output_2, ref_1, ref_2):
+    def forward(self, output_1, output_2, ref_1, ref_2, gamma):
         embeddingDist = torch.pow(self.l2norm(output_1, output_2), 2)
-        refDist = torch.pow(self.l2norm(self.referenceTransform(ref_1), self.referenceTransform(ref_2)), 2)
+        refDist = torch.pow(self.l2norm(self.referenceTransform(ref_1, gamma), self.referenceTransform(ref_2, gamma)), 2)
 
         lossTensor = torch.pow(embeddingDist - refDist, 2)
         return torch.mean(lossTensor)
 
-    def referenceTransform(self, input):
-        # return torch.div(input, torch.add(input, 1))
+    def referenceTransform(self, input, gamma):
+        #return torch.pow(input / input + 1, gamma)
         return input
