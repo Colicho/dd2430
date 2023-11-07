308535cc1d7c9420c1db16db0928394d61f5b0d5
David <david.ye01@hotmail.com>
2023-11-07 13:44:30 +0100

    updates

--- networks.py
+++ networks.py
@@ -5,29 +5,9 @@ class SiameseNetworkSimple(nn.Module):
         super(SiameseNetworkSimple, self).__init__()
         self.fc = nn.Sequential(
             nn.Linear(21, 128),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Linear(128, 64),
-            nn.ReLU(),
-            nn.Linear(64, 7)
-        )
-
-    def forward(self, x):
-        output = self.fc(x)
-        return output
-    
-
-class SiameseNetworkComplex(nn.Module):
-    def __init__(self):
-        super(SiameseNetworkComplex, self).__init__()
-        self.fc = nn.Sequential(
-            nn.Linear(21, 512),
-            nn.ReLU(),
-            nn.Linear(512, 256),
-            nn.ReLU(),
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.Linear(128, 64),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Linear(64, 7)
         )
 
