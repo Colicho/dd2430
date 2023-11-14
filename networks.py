ad1ccd7e5da52d032b9fb0b74bc10c5360f406f4
David <david.ye01@hotmail.com>
2023-11-14 11:03:38 +0100

    Added complex model + cleaning code

--- networks.py
+++ networks.py
@@ -14,4 +14,36 @@ class SiameseNetworkSimple(nn.Module):
     def forward(self, x):
         output = self.fc(x)
         return output
+
+class SiameseCNNN(nn.Module):
+    def __init__(self):
+        super(SiameseCNNN, self).__init__()
+        self.convolutional = nn.Sequential(
+            nn.Conv1d(in_channels=1, out_channels=32, kernel_size=3),
+            nn.BatchNorm1d(32),
+            nn.LeakyReLU(),
+            nn.MaxPool1d(kernel_size=2),
+            nn.Conv1d(in_channels=32, out_channels=16, kernel_size=3),
+            nn.BatchNorm1d(16),
+            nn.LeakyReLU(),
+            nn.MaxPool1d(kernel_size=2)
+        )
+        self.flatten = nn.Flatten()
+        self.fc = nn.Sequential(
+            nn.Linear(48, 64),
+            nn.BatchNorm1d(64),
+            nn.LeakyReLU(),
+            nn.Linear(64, 32),
+            nn.BatchNorm1d(32),
+            nn.LeakyReLU(),
+            nn.Linear(32, 7)
+        )
+
+    def forward(self, x):
+        x = x.unsqueeze(1)
+        x = self.convolutional(x)
+        x = self.flatten(x)
+        x = self.fc(x)
+    
+        return x
     
